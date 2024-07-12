6676c11526c669f956ce771c87ba2842140eaf3e
RAFiasyam <rafi.asyam23@gmail.com>
2024-07-12 14:45:57 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div className="w-full h-[100vh] bg-[#FEF6E4]">
+      <div className="p-[30px]">
+        <div className="flex flex-row items-center gap-[30px]">
+          <img src="https://awsimages.detik.net.id/community/media/visual/2022/03/10/ilustrasi-langit_43.jpeg?w=1200" className="w-[741px] h-[431px]" />
+          <div className="w-[600px] text-[#936A00]  ">
+            <p className="font-montserrat font-extrabold text-[20px]">Kaino.</p>
+            <h1 className="font-montserrat font-bold text-[56px]">Are you looking for good fabric for a clothing brand?</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
