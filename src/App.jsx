c11315809dc475eac58da5d3e1aa6d6a30797b3f
RAFiasyam <rafi.asyam23@gmail.com>
2024-07-12 16:28:14 +0700

    1st page done!!!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 export default function App() {
   return (
     <div className="w-full h-[100vh] bg-[#FEF6E4]">
-      <div className="p-[30px]">
+      <div className="p-[30px] flex flex-col gap-[20px]">
         <div className="flex flex-row items-center gap-[30px]">
-          <img src="https://awsimages.detik.net.id/community/media/visual/2022/03/10/ilustrasi-langit_43.jpeg?w=1200" className="w-[741px] h-[431px]" />
-          <div className="w-[600px] text-[#936A00]  ">
+          <img src="https://awsimages.detik.net.id/community/media/visual/2022/03/10/ilustrasi-langit_43.jpeg?w=1200" className="object-cover w-[728px] h-[350px]" />
+          <div className="w-[628px] text-[#936A00] flex flex-col gap-[25px] ">
             <p className="font-montserrat font-extrabold text-[20px]">Kaino.</p>
-            <h1 className="font-montserrat font-bold text-[56px]">Are you looking for good fabric for a clothing brand?</h1>
+            <h1 className="font-montserrat font-bold text-[56px] text-left">Are you looking for good fabric for a clothing brand?</h1>
+            <div className="flex flex-row gap-[62vh]">
+              <p className="font-montserrat font-extrabold text-[20px]"></p>
+              <p className="font-montserrat font-extrabold text-[20px]">Kaino.</p>
+            </div>
           </div>
+        </div>
+        <div className="flex flex-row items-center gap-[30px]">
+          <div className="w-[628px] text-[#936A00] flex flex-col gap-[65px] ">
+            <div className="flex flex-row gap-[68vh]">
+              <p className="font-montserrat font-extrabold text-[20px]"></p>
+              <p className="font-montserrat font-extrabold text-[20px]">Kaino.</p>
+            </div>
+            <h1 className="font-montserrat font-bold text-[56px] text-right">A good choice for that is 'Kaino'.</h1>
+            <p className="font-montserrat font-extrabold text-[20px]">Kaino.</p>
+          </div>
+          <img src="https://awsimages.detik.net.id/community/media/visual/2022/03/10/ilustrasi-langit_43.jpeg?w=1200" className="object-cover w-[728px] h-[350px]" />
         </div>
       </div>
     </div>
